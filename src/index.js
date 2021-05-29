654288526fc744ce52ccc07ae2bf6790d54a4d95
Mikhael <mikhaellion@gmail.com>
2021-05-29 20:09:06 +0700

    initial

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import RegisterForm from "./component/RegisterForm";
+import "../node_modules/react-bootstrap/dist/react-bootstrap.min";
+import "./index.css";
+
+const rootElement = document.getElementById("root");
+const registerElement = document.getElementById("register-root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RegisterForm />
   </React.StrictMode>,
-  document.getElementById('root')
+  registerElement
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   rootElement
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
